04fb8741c6c08b0b6200b5db3b46b8da50a05731
dojee <idojeee@gmail.com>
2025-08-21 22:59:06 +0800

    [remove] Deleted local history folder

--- .history/src/App_20250821223147.vue
+++
@@ -1,9 +0,0 @@
-<script setup></script>
-
-<template>
-  <a-layout-header></a-layout-header>
-  <a-layout-content></a-layout-content>
-  <a-layout-footer></a-layout-footer>
-</template>
-
-<style scoped></style>
--- .history/src/components/Test_20250821224240.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h2>Test</h2>
-</template>
-
-<script setup lang='ts'>
-
-</script>
-
-<style></style>
