d526944a1e4042abbe37458bea69396fd700129b
dojee <idojeee@gmail.com>
2025-08-23 00:25:42 +0800

    ll

--- src/main.js
+++ src/main.js
@@ -9,21 +9,6 @@ import router from './routers/index'
 
 
 const app = createApp(App)
-// 全局注册图标组件
-const icons = [
-    GithubOutlined,
-    GoogleOutlined,
-    MailOutlined,
-    LockOutlined,
-];
-
-icons.forEach(icon => {
-    app.component(icon.name, icon);
-});
-
-
-
-
 app.use(Antd);
 app.use(router)
 app.mount('#app')
