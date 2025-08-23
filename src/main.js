58a5d2ea5f5afc52633e03fd32faa8d9700b4ec9
dojee <idojeee@gmail.com>
2025-08-23 20:11:20 +0800

    [add] Used pinia

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import App from './App.vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import router from './routers/index'
+import { createPinia } from 'pinia'
 
 
 
 
 const app = createApp(App)
-app.use(Antd);
+const pinia = createPinia()
+app.use(Antd)
+app.use(pinia)
 app.use(router)
 app.mount('#app')
