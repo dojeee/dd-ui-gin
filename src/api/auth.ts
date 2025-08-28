bed607489136293ed7645d28c6fbf56556e2f303
dojee <idojeee@gmail.com>
2025-08-28 22:02:03 +0800

    [add] Define new api for send verify code

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,10 +4,14 @@ import { ApiResponse } from '@/types/api';
 
 // 登录接口
 export const loginApi = (params: LoginParams): Promise<ApiResponse> => {
-    return authService.post('/login', params);
+    return authService.post('/login2', params);
 };
 
 // 退出登录接口（如果后端有的话）
 export const logoutApi = () => {
     return authService.post('/logout');
+};
+
+export const sendVerificationCodeApi = (mobile: string): Promise<ApiResponse> => {
+    return authService.post('/sendVerificationCode', { mobile });
 };
