d7bd252ae20ebf9aa862aeebbdf81415e372eb4a
dojee <idojeee@gmail.com>
2025-09-02 23:01:23 +0800

    [rename] Renamed the themeStore.ts

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
-  <a-layout class="app">
-    <router-view></router-view>
-  </a-layout>
+  <a-config-provider :theme="currentTheme">
+    <a-layout class="app">
+      <router-view></router-view>
+    </a-layout>
+  </a-config-provider>
 </template>
 
-
-
 <script setup>
+
+import { theme as antTheme } from 'ant-design-vue';
+import { useThemeStore } from './stores/themeStores';
+import { computed } from 'vue';
+import { storeToRefs } from 'pinia';
+
+
+const themeStore = useThemeStore();
+const { theme } = storeToRefs(themeStore);
+
+
+
+const currentTheme = computed(() => {
+
+  if (theme.value === 'dark') {
+    return {
+      algorithm: antTheme.darkAlgorithm,
+    };
+  }
+  return {
+    algorithm: antTheme.defaultAlgorithm,
+  };
+});
 </script>
 
 
 
-<style scoped></style>
+
+
+<style scoped>
+.app {
+  min-height: 100vh;
+}
+</style>
--- src/stores/themeStores.ts
+++ src/stores/themeStores.ts
@@ -7,7 +7,6 @@ export const useThemeStore = defineStore('theme', () => {
   const theme = ref('light');
 
   function setTheme(newTheme: 'light' | 'dark') {
-    console.log("修改了theme", newTheme)
     theme.value = newTheme;
   }
 
