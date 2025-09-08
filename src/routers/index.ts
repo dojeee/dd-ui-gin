863d44d1eef87398106434fb602b8d4587adecaf
dojee <idojeee@gmail.com>
2025-09-08 19:55:07 +0800

    [add] Added role manager router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,6 +4,7 @@ import MainView from "@/views/MainView.vue";
 import { useAuthStore } from "@/stores/authStores";
 import { message } from "ant-design-vue";
 import UserManagerView from "@/views/users/UserManagerView.vue";
+import RoleManagerView from "@/views/users/RoleManagerView.vue";
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
@@ -20,13 +21,26 @@ const router = createRouter({
       meta: {
         requiresAuth: false,
       },
-    },
-    {
-      path: "/user",
-      component: UserManagerView,
-      meta: {
-        requiresAuth: false,
-      },
+      children: [
+        {
+          path: "user",
+          name: "UserManager",
+          component: UserManagerView,
+          meta: {
+            title: "UserManager",
+            requiresAuth: false,
+          },
+        },
+        {
+          path:"role",
+          name:"RoleManager",
+          component: RoleManagerView,
+          meta: {
+            title: "RoleManager",
+            requiresAuth: false,
+          },
+        }
+      ],
     },
   ],
 });
