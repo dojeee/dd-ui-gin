3b9f3af5f72e2bd804d7b4b3dd589a48f5431d3a
dojee <idojeee@gmail.com>
2025-09-24 23:59:03 +0800

    [add] Initialize the basic elements of component profile

--- src/views/profile/ProfileView.vue
+++ src/views/profile/ProfileView.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    <!-- -->
-    <h2>this is profile view</h2>
+  <div class="profile-container">
+    <div class="profile-inner-container">
+      <h2>this is profile view</h2>
+      <h2>this is profile view</h2>
+      <h2>this is profile view</h2>
+      <h2>this is profile view</h2>
+      <h2>this is profile view</h2>
+
+      <div class="profile-inner-right-container">
+        <a-avatar icon="user"> </a-avatar>
+      </div>
+      <div class="profile-inner-left-container">
+        <a-avatar icon="sss"> </a-avatar>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,4 +23,32 @@ import { ref } from "vue";
 const feature = ref();
 </script>
 
-<style lang="postcss" scoped></style>
+<style lang="scss" scoped>
+@use "@/styles/design-tokens" as v;
+
+.profile-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  padding: v.$spacing-sm v.$spacing-md;
+
+  .profile-inner-container {
+    width: 60%;
+    height: 100%;
+    padding: v.$spacing-sm v.$spacing-md;
+    background-color: red;
+    border-radius: v.$content-border-radius;
+
+    .profile-inner-left-container {
+      background-color: green;
+      float: left;
+    }
+    .profile-inner-right-container {
+      background-color: blue;
+      float: right;
+    }
+  }
+}
+</style>
