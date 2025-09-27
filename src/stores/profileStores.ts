e6abcf78549a627dd5413a92e32d2ec5d7f51e4b
dojee <idojeee@gmail.com>
2025-09-27 22:26:25 +0800

    [add] Added profile menu items to stores

--- src/stores/profileStores.ts
+++ src/stores/profileStores.ts
@@ -1,9 +1,37 @@
 import { defineStore } from "pinia";
 
 import { ref } from "vue";
+import type { IconName } from "@/assets/icons/SideBarIconMap";
+interface ProfileMenuItem {
+  key: string;
+  icon?: IconName;
+  label: string;
+  title: string;
+}
 
 export const useProfileStore = defineStore("profile", () => {
   const showModal = ref(false);
 
-  return { showModal };
+  const menuItems = ref<ProfileMenuItem[]>([
+    {
+      key: "settings",
+      label: "Settings",
+      title: "Settings",
+      icon: "UserOutlined",
+    },
+    {
+      key: "general",
+      label: "General",
+      title: "General",
+      icon: "UserOutlined",
+    },
+    {
+      key: "notification",
+      label: "Notification",
+      title: "Notification",
+      icon: "UserOutlined",
+    },
+  ]);
+
+  return { showModal, menuItems };
 });
