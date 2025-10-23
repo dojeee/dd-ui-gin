e3f6eeedaa61a12be77f3aa8901574e23fd1f174
dojee <idojeee@gmail.com>
2025-10-23 21:50:08 +0800

    [add] Added new UI components lib

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+
+
+import components from 'unplugin-vue-components/vite';
+import { AntDesignXVueResolver } from 'ant-design-x-vue/resolver';
+
+
+
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    components({
+      resolvers: [AntDesignXVueResolver()]
+    })
   ],
   resolve: {
     alias: {
